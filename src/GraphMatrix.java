2d943396177d497372906e4441e58a9e9bffe2ba
Didira123 <111513467+Didira123@users.noreply.github.com>
2023-10-22 14:42:04 -0300

    Acoes: método "rangeSearch" completo; Status: pronto para testes;

--- src/GraphMatrix.java
+++ src/GraphMatrix.java
@@ -158,20 +158,95 @@ public class GraphMatrix {
 		return true;
 	}
 	
-	public void rangeSearch(int ponto) {
+	// public int rangeSearch(int ponto) {
+	// 	// TODO translate to english
+	// 	int numComponents = 1;
+	// 	List<Integer> lista = new ArrayList<>();
+	// 	List<Integer> listaComponenteAtual = Arrays.asList(ponto);
+	// 	while(true){
+	// 		for (int i = 0; i < numVertices; i++) {
+	// 			if (lista.size() + listaComponenteAtual.size() == numVertices) {
+	// 				break;
+	// 			}
+	// 			if (ponto != i && !listaComponenteAtual.contains(i) && !lista.contains(i) && matrix[ponto][i] != 0) {
+	// 				listaComponenteAtual.add(i);
+	// 				//lista.add(i);
+	// 				ponto = i;
+	// 				i = -1;
+	// 			}
+	// 		}
+	// 		if(lista.size() + listaComponenteAtual.size() != numVertices){
+	// 			boolean newComponent = false;
+	// 			for(int i = listaComponenteAtual.size()-2; i>=0; i--){
+	// 				for(int i2 = 0; i2<numVertices; i2++){
+	// 					if (i != i2 && !listaComponenteAtual.contains(i2) && !lista.contains(i2) && matrix[ponto][i] != 0) {
+	// 						lista.addAll(listaComponenteAtual);
+	// 						listaComponenteAtual = Arrays.asList(i2);
+	// 						ponto = i2;
+	// 						newComponent = true;
+	// 						numComponents++;
+	// 						break;
+	// 					}
+	// 				}
+	// 				if(newComponent){
+	// 					break;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	return numComponents;
+	// }
+
+	public int rangeSearch(int ponto) {
 		// TODO translate to english
-		List<Integer> lista = Arrays.asList(ponto);
-		for (int i = 0; i < numVertices; i++) {
-			if (lista.size() == numVertices) {
+		int numComponents = 1;
+		List<Integer> lista = new ArrayList<>();
+		List<Integer> listaComponenteAtual = Arrays.asList(ponto);
+		while(true){
+			for (int i = 0; i < numVertices; i++) {
+				if (lista.size() + listaComponenteAtual.size() == numVertices) {
+					break;
+				}
+				if (ponto != i && !listaComponenteAtual.contains(i) && !lista.contains(i) && matrix[ponto][i] != 0) {
+					listaComponenteAtual.add(i);
+					ponto = i;
+					i = -1;
+				}
+			}
+			boolean otherWay = false;
+			if(lista.size() + listaComponenteAtual.size() != numVertices && listaComponenteAtual.size() > 1){
+				int i = listaComponenteAtual.size()-2;
+				int i2 = 0;
+				for(; i>=0; i--){
+					for(; i2<numVertices; i2++){
+						if (i != i2 && !listaComponenteAtual.contains(i2) && !lista.contains(i2) && matrix[ponto][i] != 0) {
+							otherWay = true;
+							listaComponenteAtual.add(i);
+							ponto = i2;
+							break;
+						}
+					}
+					if(otherWay){
+						break;
+					}
+				}
+			}
+			if(lista.size() + listaComponenteAtual.size() != numVertices && !otherWay){
+				numComponents++;
+				lista.addAll(listaComponenteAtual);
+				for(int cont = 0; cont<numVertices; cont++){
+					if(!lista.contains(cont)){
+						listaComponenteAtual = Arrays.asList(cont);
+						ponto = cont;
+						break;
+					}
+				}
+			}
+			if(lista.size() + listaComponenteAtual.size() == numVertices){
 				break;
 			}
-			if (!lista.contains(i) && ponto != i && matrix[ponto][i] != 0) {
-				lista.add(i);
-				ponto = i;
-				i = -1;
-			}
 		}
-		
+		return numComponents;
 	}
 
 	// TODO format as table
