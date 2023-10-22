5e00841609575e3a340688ea7a04823915eee312
Felipe Campelo <iifcamp@icloud.com>
2023-10-22 10:57:17 -0300

    22 october @Felipe ADD boolean isDirected, isWeighted and setMatrixType FIX constructors (random matrix and input matrix) - now automatically set their matrixType FIX randomize() - now iterates always through the LOWER TRIANGLE (instead of the whole matrix) FIX matrixType - now concerns 4 types of matrices (instead of 2) inclding weighted ones.

--- src/GraphMatrix.java
+++ src/GraphMatrix.java
@@ -8,64 +8,136 @@ public class GraphMatrix {
 
 	private int[][] matrix;
 	private int 	numVertices;
+	private int 	matrixType;
 	private boolean isDirected;
 	private boolean isWeighted;
 	
-	public GraphMatrix(int numVertices) {
+	/**Random matrix
+	 * Receives boolean atributes to set the matrix type
+	 * Sets the matrix type automaticall, then randomizes the edges
+	 * 
+	 * @param numVertices int
+	 * @param isDirected boolean
+	 * @param isWeighted boolean
+	 */
+	public GraphMatrix(int numVertices, boolean isDirected, boolean isWeighted) {
 		this.numVertices = numVertices;
 		matrix = new int[numVertices][numVertices];
+		this.isDirected = isDirected;
+		this.isWeighted = isWeighted;
+		setMatrixType();
+		randomize();
 	}
 
-	public GraphMatrix(int numVertices, int[][] novaMatriz) {
+	/**Matrix from input
+	 * Client inputs an array as a matrix
+	 * Atributes are set automatically 
+	 * @param numVertices int
+	 * @param newMatrix int[][]
+	 */
+	public GraphMatrix(int numVertices, int[][] newMatrix) {
 		this.numVertices = numVertices;
-		matrix = novaMatriz;
+		matrix = newMatrix;
+		setIsDirected();
+		setIsWeighted();
+		setMatrixType();
 	}
 	
+	/**Sets the edges according to the matrixType
+	 * Always iterating through the LOWER TRIANGLE. 
+	 * Instead of O(n^2), it's O(n(n-1)/2) (i.e. polynomial instead of quadratic)
+	 */
 	public void randomize() {
-		switch (tipoMatriz) {
-
-			/* TODO implement this function with the new bool atributes
-   			   type 	cells     	  triangles
-   			!dir !wei ... 0s and 1s ... 	mirror 
-       			!dir  wei ... any num   ... 	mirror
-	  		 dir !wei ... 0s and 1s ... (a -> b: x; b -> a: 0)
-      			 dir  wei ... any num 	... (a -> b: x; b -> a: 0)
-	     		*/
+		
+		switch (matrixType) {
+			case 0: // !dir !wei 	0s and 1s 	mirror
 				
-			case 0: 
-				for (int i = 0; i < numVertices; i++) {
+			// iterates through the LOWER TRIANGLE,
+				for (int i = 1; i < numVertices; i++) {
 					for (int j = 0; j != i; j++) {
+
+						// randomizes the edges (0 or 1)
 						int value = rnd.nextInt(0, 2);
+
+						// mirrors the edges
 						matrix[i][j] = value;
 						matrix[j][i] = value;
 					}
 				}
-				break;
+			break;
+
+			case 1: // !dir  wei 	any num   	mirror
 			
-			case 1: // todo -1 desnecessário (adjacencia)
-				for (int i = 0; i < numVertices; i++) {
+			// iterates through the LOWER TRIANGLE,
+				for (int i = 1; i < numVertices; i++) {
 					for (int j = 0; j < i; j++) {
-						int value = rnd.nextInt(-1, 2);
+
+						// randomizes the edges (any num)
+						int value = rnd.nextInt();
+
+						// mirrors the edges
 						matrix[i][j] = value;
-						matrix[j][i] = -value;
+						matrix[j][i] = value;
 					}
 				}
-				break;
+			break;
+
+			case 2: // dir !wei 	0s and 1s 	(a -> b: 1; b -> a: 0)
 			
-			default:
-				for (int i = 0; i < numVertices; i++) {
-					for (int j = 0; j < numVertices; j++) {
-						if (i != j) {
-							matrix[i][j] = rnd.nextInt();
+			// iterates through the LOWER TRIANGLE,
+				for (int i = 1; i < numVertices; i++) {
+					for (int j = 0; j != i; j++) {
+
+						// randomizes in which triangle to fill
+						int orientation = rnd.nextInt(0, 2);
+
+						switch (orientation) {
+							case 0: // conventional IJ
+								matrix[i][j] = 1;
+								break;
+							case 1: // flipped JI
+								matrix[j][i] = 1;
+								break;
+							default:
+							break;
 						}
+						
 					}
 				}
-				break;
+			break;
+
+			case 3: // dir  wei 	any num    	(a -> b: x; b -> a: 0)
+			
+			// iterates through the LOWER TRIANGLE,
+				for (int i = 1; i < numVertices; i++) {
+					for (int j = 0; j != i; j++) {
+
+						// randomizes in which triangle to fill and the value
+						int orientation = rnd.nextInt(0, 2);
+						int value = rnd.nextInt();
+
+						switch (orientation) {
+							case 0: // conventional IJ
+								matrix[i][j] = value;
+								break;
+							case 1: // flipped JI
+								matrix[j][i] = value;
+								break;
+							default:
+							break;
+						}
+						
+					}
+				}
+			break;
+			default:
+			break;
 
 		}
 	}
 	
-	public boolean isGrafoCompleto() {
+	// Algorithms
+	public boolean isComplete() {
 		
 		if (numVertices == 1) {
 			return false;
@@ -81,13 +153,13 @@ public class GraphMatrix {
 		return true;
 	}
 	
-	public boolean possuiIsomorfismo(int[][] outraMatriz) {
-		//		asd
-		// TODO Buscar/Criar o código deste método
+	public boolean areIsomorphs(int[][] outraMatriz) {
+		// TODO implement
 		return true;
 	}
 	
 	public void rangeSearch(int ponto) {
+		// TODO translate to english
 		List<Integer> lista = Arrays.asList(ponto);
 		for (int i = 0; i < numVertices; i++) {
 			if (lista.size() == numVertices) {
@@ -102,8 +174,7 @@ public class GraphMatrix {
 		
 	}
 
-	/* TODO Formatar uma tabela para a exibição de fácil leitura...
-	 	como não tem mais -1 talvez nem seja necessario */
+	// TODO format as table
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -118,14 +189,60 @@ public class GraphMatrix {
 		return builder.toString();
 	}
 	
-	// Get and Set
-	public int getTipoMatriz() {
-		return tipoMatriz;
-	}
+	// Get Set
+	private void setMatrixType() {
+
+		int type;
+		/* Types			Values		Triangles
+		 * 0: !dir !wei 	0s and 1s 	mirror
+		 * 1: !dir  wei 	any num   	mirror
+		 * 2:  dir !wei 	0s and 1s 	(a -> b: 1; b -> a: 0)
+		 * 3:  dir  wei 	any num    	(a -> b: x; b -> a: 0)
+		 */
+		if (!isDirected && !isWeighted) {
+			type = 0;
+		} else if (!isDirected && isWeighted) {
+			type = 1;
+		} else if (isDirected && !isWeighted) {
+			type = 2;
+		} else {
+			type = 3;
+		}
+		
+		this.matrixType = type;
+}
+
+	private void setIsDirected() {
 	
-	public void setTipoMatriz(int tipoMatriz) {
-		this.tipoMatriz = tipoMatriz;
+		// these two loops iterate through the LOWER TRIANGLE,
+		// EXCLUDING the MAIN DIAGONAL.
+		for (int i = 1; i < numVertices; i++)
+			for (int j = 0; j < i; j++)
+			
+				// if any cells are NOT MIRRORED,
+				if (matrix[i][j] != matrix[j][i]) {
+				
+					// then isDirected = TRUE
+					this.isDirected = true;
+	       			return;
+				}
 	}
+
+	private void setIsWeighted() {
+	
+	// iterates through the ENTIRE matrix
+	for (int i = 0; i < numVertices; i++)
+		for (int j = 0; j < numVertices; j++)
+			
+			// checks for cells that are neither 0 nor 1,
+			// !0 and !1 == !(0 or 1)
+			if ( !(matrix[i][j] == 0 || matrix[i][j] == 1) ) {
+				
+				// then isWeighted = TRUE
+				this.isWeighted = true;
+				return;
+			}
+	} 
 	
 	public int[][] getMatrix() {
 		return matrix;
