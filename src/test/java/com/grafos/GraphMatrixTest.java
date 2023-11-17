72bff62ba05cfccc8364bae10cc904f1d72e7d6d
feed0 <dogmas-gutters.07@icloud.com>
2023-11-17 12:05:05 -0300

    @feed #9 having problems with testing

--- src/test/java/com/grafos/GraphMatrixTest.java
+++ src/test/java/com/grafos/GraphMatrixTest.java
@@ -1,5 +1,6 @@
 package com.grafos;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -37,32 +38,65 @@ public class GraphMatrixTest {
         }
 
         @Test
-        public void toStringTest() {
-
+        public void depthSearch() {
                 GraphMatrix gm;
                 int[][] matrix;
-                String expected;
 
+                // 8 vertices
+                /*      0: 2,3
+                        1: 3, 5, 6
+                        2: 0, 4, 5
+                        3: 0, 1, 5, 7
+                        4: 2
+                        5: 1, 2, 3
+                        6: 1, 7
+                        7: 3, 6
+                */
                 matrix = new int[][] {
-                                { 0, 1, 1, 1 },
-                                { 1, 0, 1, 1 },
-                                { 1, 1, 0, 1 },
-                                { 1, 1, 1, 0 }
+                        { 0, 0, 1, 1, 0, 0, 0, 0 },
+                        { 0, 0, 0, 1, 0, 1, 1, 0 },
+                        { 1, 0, 0, 0, 1, 1, 0, 0 },
+                        { 1, 1, 0, 0, 0, 1, 0, 1 },
+                        { 0, 0, 1, 0, 0, 0, 0, 0 },
+                        { 0, 1, 1, 1, 0, 0, 0, 0 },
+                        { 0, 1, 0, 0, 0, 0, 0, 1 },
+                        { 0, 0, 0, 1, 0, 0, 1, 0 }
                 };
-
-                expected = "0 1 1 1 \n" +
-                                "1 0 1 1 \n" +
-                                "1 1 0 1 \n" +
-                                "1 1 1 0 \n";
-
                 gm = new GraphMatrix(matrix);
-                assertEquals(gm.toString(), expected);
-
+                
+                int expected = 1;
+                int actual = gm.depthSearch(2);
+                
+                assertEquals(expected, actual);
         }
 
         @Test
         public void prim() {
 
-               // TODO
+                GraphMatrix gm;
+                int[][] matrix;
+                int[] expected;
+
+                // 6 vertices graph with the following edges:
+                // ab4 ac2 ae3
+                // bd5 cd1 ce6
+                // df6 ef2
+                matrix = new int[][] {
+                                { 0, 4, 2, 0, 3, 0 },
+                                { 4, 0, 0, 5, 0, 0 },
+                                { 2, 0, 0, 1, 6, 3 },
+                                { 0, 5, 1, 0, 0, 6 },
+                                { 3, 0, 6, 0, 0, 2 },
+                                { 0, 0, 3, 6, 2, 0 }
+                };
+                gm = new GraphMatrix(matrix);
+
+                int[] actual = gm.prim(0);
+
+                // Previous = {0, a, a, c, a, e}
+                expected = new int[] { 0, 0, 0, 2, 0, 4 };
+
+                // assert expected == gm.prim
+                assertArrayEquals(expected, actual);
         }
 }
